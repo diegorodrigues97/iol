7f17d6b257c127ba5672fb0af043564c1e52a24c
Diego Rodrigues da Silva <diegorodrigues97@yahoo.com.br>
2023-12-16 11:14:28 -0300

    chore: set initial structure

---
+++ src/main/java/br/com/diegords/IolApplication.java
@@ -0,0 +1,12 @@
+package br.com.diegords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IolApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(IolApplication.class, args);
+    }
+}
